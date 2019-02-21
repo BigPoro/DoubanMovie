ddbd60b94fd241a96d3af8ac46331618f23fef0b
胖虎 <idoghuan@ifeixiu.com>
2019-02-21 21:30:47 +0800

    1.导入资源文件 2. 配置cocoapods

---
+++ DoubanMovie/DoubanMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoubanMovie
+//
+//  Created by iDog on 2019/2/21.
+//  Copyright © 2019 iDog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
